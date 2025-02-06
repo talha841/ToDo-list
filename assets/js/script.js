d6a8fe045231bb68c009cac6ed05bd664076177f
Talha liaqat <liaqattalha45@gmail.com>
2025-02-06 14:52:52 +0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+let btn1 = document.querySelector('button')
+let inp = document.querySelector('#text')
+let ol = document.querySelector('ol')
+// ol.style.listStyleType = 'upper-roman'
+
+
+btn1.onclick=()=>{
+      let li = document.createElement('li')
+      if (inp.value == '') {
+            alert('please enter something')
+      } else {
+            li.textContent = inp.value
+            let btn2 = document.createElement('button')
+            li.style.marginLeft = '35%'
+            li.style.marginTop = '10px'
+      btn2.textContent = 'delete'
+       btn2.style.marginLeft = '30px'
+            li.appendChild(btn2)
+            ol.appendChild(li)
+           ol.style.listStyleType = 'upper-roman'
+            btn2.addEventListener('click', ()=>{
+                  li.remove()
+                  
+            })
+            inp.value = ''
+      }      
+}
+btn1.addEventListener('click', (e)=>{
+      e.preventDefault()
+})
